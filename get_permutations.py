6af8811dfecb1b86cdc0b190a95b53c85d6c3566
shine_wxf <127000639+wShinef@users.noreply.github.com>
2024-11-13 22:18:14 +0800

    Create get_permutations.py

---
+++ get_permutations.py
@@ -0,0 +1,24 @@
+"""
+输出给定字符串的全部排列
+"""
+
+def get_permutations(s: str):
+    if len(s) == 0:
+        return
+    if len(s) == 1:
+        return [s]
+
+    result = []
+    for i in range(len(s)):
+        c = s[i]
+        ros = s[:i] + s[i+1:]
+
+        for p in get_permutations(ros):
+            result.append(c+ros)
+
+    return result
+
+
+if __name__ == '__main__':
+    str1 = 'abcd'
+    print(get_permutations(str1))
